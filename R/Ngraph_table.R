d84b42ec42f72a1be17bccee05030cd433def291
Minjee Kim <minjeekim0804@tamu.edu>
2022-11-23 18:15:57 -0600

    year compatibility for inputting the inputs into the RNHANES format for data retrieval

--- R/Ngraph_table.R
+++ R/Ngraph_table.R
@@ -2,13 +2,44 @@
 
 ## call data using rnhanes
 ## outputs the data in a workable dataframe format
-# nhanes_table <- function(year, ){
-#   if(null(year) == T ){
-#     stop("Null input for year!")
-#   }else if(is.numeric(year) == T){
-#     stop("The value(s) inputted for year is not numeric")
-#   }
-# }
+
+
+## create a function that converts the inputted year into a range of years (in string) that is compatible with the RNHANES format
+
+
+## accept a year range that is more flexible
+## not sensitive to upper/lower case
+
+## year is the range of cycles
+#' Title
+#'
+#' @param year String vector that contains a range of years or a specific year
+#' @param variables A list or a specific variable the user is interested in. Search for a specific variable using the "nhanesA::nhanesTableVars('EXAM', 'BMX_D')"
+#'
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nhanes_table <- function(year, type, variables){
+  if(null(year) == T ){
+    stop("empty input for year!")
+  }else{
+    if(grepl("-",year, ignore.case = T) == T){
+
+    }else{
+      year = as.numeric(year)
+      if(year/2 == 0){
+
+      }
+    }
+
+  }
+
+
+
+  nhanes_load_data(type, year)
+}
 
 ## output the dataframe into a publication ready format
 ##
