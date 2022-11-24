75ed219342b50b7cf089f760d03f9418a9d4a75e
Minjee Kim <minjeekim0804@tamu.edu>
2022-11-23 19:03:55 -0600

    initiated the data portion

--- R/Ngraph_table.R
+++ R/Ngraph_table.R
@@ -4,14 +4,14 @@
 ## outputs the data in a workable dataframe format
 
 
-## create a function that converts the inputted year into a range of years (in string) that is compatible with the RNHANES format
+##### purpose: create a function that converts the inputted year into a range of years (in string) that is compatible with the RNHANES format
 
-
+## RNHANES only accpets years that are
 ## accept a year range that is more flexible
 ## not sensitive to upper/lower case
 
 ## year is the range of cycles
-#' Title
+#' Create a data frame with specified parameters
 #'
 #' @param year String vector that contains a range of years or a specific year
 #' @param variables A list or a specific variable the user is interested in. Search for a specific variable using the "nhanesA::nhanesTableVars('EXAM', 'BMX_D')"
@@ -21,16 +21,18 @@
 #' @export
 #'
 #' @examples
-nhanes_table <- function(year, type, variables){
+nhanes_table <- function(year, variables){
+
   if(null(year) == T ){
     stop("empty input for year!")
   }else{
-    if(grepl("-",year, ignore.case = T) == T){
-
+    if(grepl("-*",year, ignore.case = T) == T){
+      first <- sub("-", "", a)
     }else{
+      ## if the user inputted year as a single year
       year = as.numeric(year)
       if(year/2 == 0){
-
+        year = paste(year, "-", year-1, sep = "")
       }
     }
 
@@ -42,7 +44,17 @@ nhanes_table <- function(year, type, variables){
 }
 
 ## output the dataframe into a publication ready format
-##
-# Ngraph_table <- function(){
-#
-# }
+
+#' Title
+#'
+#' @inheritParams nhanes_table
+#' @param year
+#' @param variables
+#'
+#' @return
+#'
+#'
+#' @examples
+Ngraph_table <- function(year, variables){
+
+}
