093385d1d40495c2f4a01f8a56b48cceb5709b77
Minjee Kim <minjeekim0804@tamu.edu>
2022-11-23 19:03:37 -0600

    initiated skeleton for the graphing portion

--- R/Ngraph_shiny.R
+++ R/Ngraph_shiny.R
@@ -1,3 +1,20 @@
 
 
 ### interactive plots using the Shiny package
+
+## data: data frame constructed via ngraph_table
+## this function creates: histogram, line graph, or scatter plot, specified by plot
+## user can input a long range of years to create graph over time
+## if the specified time cycle is only one cycle, it degenerates to a basic graph (histogram, line graph, etc... )
+#' Interactive Plotting with Shiny
+#'
+#' @param data If the inputted data contains more than one cycle, this function generates a graph over time
+#' @param variable User can specify the variables to plot with this function.
+#' @param graph Type of graph the user wishes to generate
+#'
+#' @return
+#'
+#' @examples
+Ngraph <- function(data, variable, graph){
+
+}
