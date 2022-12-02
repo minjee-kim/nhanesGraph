93c723f88270b4a5e34f23accd9f7c2b6e670d20
Minjee Kim <minjeekim0804@tamu.edu>
2022-12-02 15:31:59 -0600

    emtpy file

--- R/Ngraph_shiny.R
+++ R/Ngraph_shiny.R
@@ -6,15 +6,3 @@
 ## this function creates: histogram, line graph, or scatter plot, specified by plot
 ## user can input a long range of years to create graph over time
 ## if the specified time cycle is only one cycle, it degenerates to a basic graph (histogram, line graph, etc... )
-#' Interactive Plotting with Shiny
-#'
-#' @param data If the inputted data contains more than one cycle, this function generates a graph over time
-#' @param variable User can specify the variables to plot with this function.
-#' @param graph Type of graph the user wishes to generate
-#'
-#' @return
-#'
-#' @examples
-Ngraph <- function(data, variable, graph){
-
-}
