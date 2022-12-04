88efaaadc5c10c5ae80e6b6e4a574356bf3b0085
Minjee Kim <minjeekim0804@tamu.edu>
2022-12-03 18:08:19 -0600

    documentation for the stored data (nhanes_file_list)

---
+++ R/nhanes_file_list.R
@@ -0,0 +1,6 @@
+
+#' This is data to be included in my package
+#'
+#' @author Minjee Kim \email{minjeekim0804@tamu.edu}
+#' @references \url{https://cran.r-project.org/web/packages/RNHANES/vignettes/introduction.html}
+"nhanes_file_list"
