ce83f9a370e90d35e7f224fdb7fbac57e75568f9
Minjee Kim <minjeekim0804@tamu.edu>
2022-12-03 18:09:16 -0600

    created a shiny app that allows nhanes data file search

--- R/nGraph_search.R
+++ R/nGraph_search.R
@@ -4,31 +4,20 @@
 ## building a shiny app for interactive search
 ## search by inputs : cycle, query, variable name
 
-nhanes_file_list = RNHANES::nhanes_data_files()
-usethis::use_data(nhanes_file_list, compress = "xz")
+# nhanes_file_list = RNHANES::nhanes_data_files()
+# usethis::use_data(nhanes_file_list, compress = "xz")
 
 
-
-myApp <- function(...) {
-  ui <- fluidPage(
-    fluidRow(
-      column(3,
-             selectInput("cycle", h3("Choose a cycle:"),
-                         choices =  RNHANES::nhanes_cycle_years(), selected = 1)),
-
-      column(3,
-             selectInput("component", h3("Choose a component:"),
-                         choices = list("all" = 1, "demographics" = 2,
-                                        "dietary" = 3), selected = 1)),
-      tableOutput("result")
-
-    )
-  )
-  server <- function(input, output, session) {
-    output$result <- renderTable({
-      as.data.frame(RNHANES::nhanes_search(nhanes_file_list, input$component, cycle == input$cycle))
-    })
+#' run shiny app with nhanesGraph package
+#'
+#' @return shiny app
+#' @export
+#'
+nGraph_shiny <- function() {
+  appDir <- system.file("nGraph_search", "myapp", package = "nhanesGraph")
+  if (appDir == "") {
+    stop("Could not find the directory. Try re-installing `nhanesGraph`.", call. = FALSE)
   }
-  shinyApp(ui, server)
-}
 
+  shiny::runApp(appDir, display.mode = "normal")
+}
