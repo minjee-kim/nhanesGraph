fbf351994eededd2bf3c9c655b2e4aa7fee41d99
Minjee Kim <minjeekim0804@tamu.edu>
2022-12-05 21:52:01 -0600

    added a search bar to the shiny app

--- inst/nGraph_search/myapp/server.R
+++ inst/nGraph_search/myapp/server.R
@@ -2,8 +2,15 @@
 
 ## server function for nhanes_shiny app
 
-server <- function(input, output, session) {
+server <- function(input, output, session){
+
   output$result <- renderTable({
-    as.data.frame(RNHANES::nhanes_search(nhanes_file_list, input$component, cycle == input$cycle))
+    # data = as.data.frame(nhanes_file_list[which(sapply(nhanes_file_list, grepl, pattern = input$searchme)),])
+    data = nhanes_file_list
+    as.data.frame(RNHANES::nhanes_search(data, input$component, cycle == input$cycle))
   })
+
 }
+
+
+
--- inst/nGraph_search/myapp/ui.R
+++ inst/nGraph_search/myapp/ui.R
@@ -2,6 +2,7 @@
 ## user interface for shiny app
 
 ui <- fluidPage(
+  titlePanel("Search NHANES files with nhanesGraph Package"),
   shinyWidgets::setBackgroundColor(
     color = c("#FFF0F5")
   ),
@@ -11,10 +12,18 @@ ui <- fluidPage(
            selectInput("cycle", h3("Choose a cycle:"),
                        choices =  RNHANES::nhanes_cycle_years(), selected = 1)),
 
-    column(3,
+    column(2,
            selectInput("component", h3("Choose a component:"),
                        choices = list("all" = 1, "demographics" = 2,
-                                      "dietary" = 3), selected = 1))
+                                      "dietary" = 3, "examination" = 4,
+                                      "laboratory" = 5,  "questionnaire" = 6), selected = 1))
+    # column(3,
+    #   selecInput(
+    #     inputId = "searchme",
+    #     label = "Search Bar",
+    #     multiple = FALSE,
+    #     choices = c("Search Bar" = "", paste0(LETTERS,sample(LETTERS, 26))),
+    #   ))
 
   ),
   tableOutput("result"),
