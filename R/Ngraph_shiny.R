2fcc12bef472f2db1f88e4801cb0954673278e35
Minjee Kim <minjeekim0804@tamu.edu>
2022-12-09 09:37:50 -0600

    started writing the nGraph_shiny shiny app

--- R/Ngraph_shiny.R
+++ R/Ngraph_shiny.R
@@ -6,3 +6,18 @@
 ## this function creates: histogram, line graph, or scatter plot, specified by plot
 ## user can input a long range of years to create graph over time
 ## if the specified time cycle is only one cycle, it degenerates to a basic graph (histogram, line graph, etc... )
+
+#' Title
+#'
+#' @return nGraph_shiny Shiny app
+#' @export
+#'
+#' @examples
+nGraph_shiny <- function() {
+  appDir <- system.file("nGraph_shiny", "MyApp", package = "nhanesGraph")
+  if (appDir == "") {
+    stop("Could not find the directory. Try re-installing `nhanesGraph`.", call. = FALSE)
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+}
---
+++ inst/nGraph_shiny/MyApp/App.R
@@ -0,0 +1,83 @@
+
+
+
+ui <- fluidPage(
+
+  ## exit button
+  tags$button(
+    id = 'close',
+    type = "button",
+    class = "btn action-button",
+    onclick = "setTimeout(function(){window.close();},500);",  # close browser
+    "Close window"
+  ),
+
+  ## title and background
+  titlePanel("Visualize NHANES data with nhanesGraph Package"),
+  shinyWidgets::setBackgroundColor(
+    color = c("#f2f0ff")
+  ),
+
+  ## user inputs
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Visualize NHANES files by variables."),
+
+      selectInput("cycle", h3("Choose a cycle:"),
+                  choices =  RNHANES::nhanes_cycle_years(), selected = 1),
+
+      selectInput("component", h3("Choose a component:"),
+                  choices = list("demographics",
+                                 "dietary", "examination",
+                                 "laboratory",  "questionnaire" )),
+    uiOutput("variable")),
+    mainPanel(
+      tableOutput("plot")
+    )
+
+  )
+)
+
+
+## server function for nhanes_shiny app
+
+server <- function(input, output){
+  observe({
+    if (input$close > 0) stopApp()                             # stop shiny
+  })
+
+  ### creating a selectInput for variable list, depending on the component and cycle inputs
+  output$variable <- renderUI({
+
+    ## get the corresponding variable name list by cycle and component inputs
+    var_list = nhanes_variable_list[nhanes_variable_list$cycle == input$cycle,]
+    var_list = var_list[var_list$component == input$component,]
+    variable_des <- as.vector(var_list$variable_description)
+    variable_name <- var_list[var_list$variable_description == variable_des,]$variable_name
+    variable_list <- paste(paste("(", variable_name, ")", sep = ""), variable_des, sep = " ")
+    # render selectizeInput
+    selectizeInput("variable", "Variables",
+                   choices = c(variable_list))
+  })
+
+  output$plot <- renderTable({
+
+    ## filter with the cycle first
+    data = nhanes_variable_list[nhanes_variable_list$cycle == input$cycle,]
+
+    ## getting the variable name from the input
+    var = paste(gsub("\\(*", "", (gsub(").*$", "", input$variable))))
+
+    ## get the file name using the variable name
+    file = data[data$variable_name == var, ]$data_file_name
+
+    data = nhanes_table(input$cycle, file)
+    data
+  })
+
+
+}
+
+
+
+shinyApp(ui, server)
