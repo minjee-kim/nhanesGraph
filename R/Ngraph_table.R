eb78633b01efce351f455374727cff00863c8a71
Minjee Kim <minjeekim0804@tamu.edu>
2022-12-09 09:34:52 -0600

    edited nhanes_table function to include nGraph_search shiny app when needed.

--- R/Ngraph_table.R
+++ R/Ngraph_table.R
@@ -52,105 +52,134 @@ year_check <- function(first, second){
 #' Loading NHANES data with a flexible year input
 #'
 #' @param year a numeric value of a year or string objective of a range year
-#' @param type type of files
-#' @param variables a list or a specific variable the user is interested in. Search for a specific variable using the "nhanesA::nhanesTableVars('EXAM', 'BMX_D')"
-#' @param demographics logical value - whether to include a demographic information or not
+#' @param file_name name of the file that the user wishes to load (required)
+#' @param demographics logical value - whether to include a demographic information (inherited from RNHANES::nhanes_search())
+#' @param recode logical value - whether to recode the categorical variables into integers (inherited from RNHANES::nhanes_search())
 #'
 #' @return an NHANES data table with user specified parameters
 #' @export
 #'
-#' @examples nhanes_table("2007-2008","EPH")
-nhanes_table <- function(year, type = NULL, variables = NULL, demographics = F){
+#' @examples nhanes_table("2009","EPH")
+nhanes_table <- function(year = NULL, file_name = NULL, demographics = F, recode = F){
 
-  ## making sure the year input isn't empty
-  if(is.null(year) == T ){
-    stop("empty input for year!") # stopping the code if year is empty
+  ## if the file name input is empty, the function displays nGraph_search Shiny app to help the user explore the options.
+  if(is.null(file_name)){
+    cat("File name is required! Please explore the NHANES files with nhanesGraph::nGraph_search().")
+    ## ask the user to choose from two options
+    user_ans = readline(prompt = "Press a if you'd like to see the nGraph_search Shiny app to explore the file options or q if you'd like to exit. \n Please exit nGraph_search by clicking the close window button on the top left to avoid conflict.")
+    if(user_ans == "q"){
+      print("exited from nhanes_table.")
+      stop("")
+    }else if(user_ans == "a"){
+      nGraph_search()
+      stop()
+    }else{
+        stop("Please choose from one of the options!")
+      }
+
   }else{
-    ## if there is a single value for the year, output a message that indicates the cycle
+    ## if the file_name is present
+    ## making sure the year input isn't empty
+    if(is.null(year) == T ){
+      stop("working on it:)")
+    }else{
+      ## if there is a single value for the year, output a message that indicates the cycle
 
-    ## for numeric inputs, make it into a cycle (string) to input into RNHANES
-    if(is.numeric(year)){
-      if(year < 1999 || year > 2021){ stop("The continuous NHANES starts from 1999 and is only available up to 2020. Please input a valid year or a valid cycle.") }
-      if(year %% 2 == 1){
-        input_year = paste(year, year + 1 , sep = "-")
-      }else{input_year = paste(year - 1, year, sep = "-")}
-
-    }else{ if(is.character(year)){
-      ## for string inputs, check whether the input is a cycle or a specific year
-
-      ## check for single year input
-      if(grepl("-*",year) == F){
-        temp = as.numeric(year)
-        ## check whether the inputted year is valid
-        if(temp < 1999 || temp > 2020){
-          ## make the single input into a cycle
-          ## if the inputted year is odd, add one and make it into a cycle
-          if(temp %% 2 == 1){
-            input_year = paste(temp, temp + 1, sep = "-")
-          }else{
-            input_year = paste(temp - 1, temp, sep = "-")
-          }
+      ## for numeric inputs, make it into a cycle (string) to input into RNHANES
+      if(is.numeric(year)){
+        if(year < 1999 || year > 2021){
+          stop("The continuous NHANES starts from 1999 and is only available up to 2020. Please input a valid year or a valid cycle.")
+        }else{
+          if(year %% 2 == 1){
+            input_year = paste(year, year + 1 , sep = "-")
+          }else{input_year = paste(year - 1, year, sep = "-")}
         }
-        ## end of string single year input
+        ## end of numeric year input
       }else{
-        ## check for cycle input
-          first <- as.numeric(sub("\\-.*", "", year)) # get the first part of the specified year range
-          last  <- as.numeric(sub(".*-", "", year)) # get the last part of the specified year range
+        if(is.character(year)){
+          ## for string inputs, check whether the input is a cycle or a specific year
 
-          #### check whether this is a valid cycle
-          ## check if first is greater than 1999 and less than 2020
-          if(first < 1999 || first > 2020 || last > 2020){
-            stop("The continuous NHANES starts from 1999 and is only available up to 2020. Please input a valid year or a valid cycle.")
-          }else{
-            ## check if first is one less than second
-
-            if(first > last){
-              print("The first part of your year input is larger than the second part. Please input a valid cycle or a specific year you would like to see.")
-              stop()
-            }else{
-              ## the inputted string cycle is correct
-              if(first == last - 1){
-                input_year = year
+          ## check for single year input
+          if(grepl("-",year) == F){
+            temp = as.numeric(year)
+            ## check whether the inputted year is valid
+            if(temp > 1999 && temp < 2020){
+              ## make the single input into a cycle
+              ## if the inputted year is odd, add one and make it into a cycle
+              if(temp %% 2 == 1){
+                input_year = paste(temp, temp + 1, sep = "-")
               }else{
-                ## ask which cycle they want to see
-                cat("It seems like your year input contains multiple cycles. Which cycle would you like to see?", sep = '\n')
-                cat('\n')
-                cycles = year_check(first, last)
-                cat(cycles, sep = '\n')
+                input_year = paste(temp - 1, temp, sep = "-")
+              }
+            }
+            ## end of string single year input
+          }else{
+            if(grepl("-",year) == T){
+              ## check for cycle input
+              first <- as.numeric(sub("\\-.*", "", year)) # get the first part of the specified year range
+              last  <- as.numeric(sub(".*-", "", year)) # get the last part of the specified year range
 
-                ## ask the user to choose from the possible cycles
-                user_ans = readline(prompt = "Choose a cycle! Press q if you'd like to stop. ")
-                if(user_ans == "q"){
-                  print("safely stoped.")
-                  stop("\r ")
+              #### check whether this is a valid cycle
+              ## check if first is greater than 1999 and less than 2020
+              if(first < 1999 || first > 2020 || last > 2020){
+                stop("The continuous NHANES starts from 1999 and is only available up to 2020. Please input a valid year or a valid cycle.")
+              }else{
+                ## check if first is one less than second
+
+                if(first > last){
+                  print("The first part of your year input is larger than the second part. Please input a valid cycle or a specific year you would like to see.")
+                  stop()
                 }else{
-                  if(is.numeric(user_ans) == F || user_ans > length(cycles) || user_ans < 1){
-                    print("Please provide an integer from the given options.")
-                    stop("\r ")
+                  ## the inputted string cycle is correct
+                  if(first == last - 1){
+                    input_year = year
                   }else{
-                    ## get the user chosen cycle
-                    input_year = paste(sub('.*\\.', "", cycles[user_ans]))
+                    ## ask which cycle they want to see
+                    cat("It seems like your year input contains multiple cycles. Which cycle would you like to see?", sep = '\n')
+                    cat('\n')
+                    cycles = year_check(first, last)
+                    cat(cycles, sep = '\n')
+
+                    ## ask the user to choose from the possible cycles
+                    user_ans = readline(prompt = "Choose a cycle! Press q if you'd like to exit. ")
+                    if(user_ans == "q"){
+                      print("safely stoped.")
+                      stop("")
+                    }else{
+                      user_ans = as.numeric(user_ans)
+                      if(is.numeric(user_ans) == F || user_ans > length(cycles) || user_ans < 1){
+                        print("Please provide an integer from the given options.")
+                        stop("")
+                      }else{
+                        ## get the user chosen cycle
+                        input_year = paste(sub('.*\\.', "", cycles[user_ans]))
+                      }
+                    }
                   }
                 }
+
               }
             }
 
-       }
+          } ## end of string part of year checking
+        } ## end of string part of year checking
 
-      } ## end of string part of year checking
-    } ## end of string part of year checking
-
-  } ## end of function
+      } ## end of non-numeric year input
 
 
-  } ## end of non-null inputs
-  ## input the cycles into the RNHANES command to retrieve the data of interest
-  # nhanes_load_data(file_name = type, year = input_year, demographics = demographics)
-  RNHANES::nhanes_load_data(file_name = "EPH", year = input_year, demographics = demographics)
+    } ## end of non-null year inputs
 
-  ## filter the data for the specific variables
+
+    ## input the cycles into the RNHANES command to retrieve the data of interest
+    RNHANES::nhanes_load_data(file_name = file_name, year = input_year, demographics = demographics, recode = recode)
+  }
 }
 
 ## output the dataframe into a publication ready format
+
+
+
+
+
 
 
