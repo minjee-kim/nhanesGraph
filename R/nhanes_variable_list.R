fc23d48e580fa462a603d7091a154df7309a1aa4
Minjee Kim <minjeekim0804@tamu.edu>
2022-12-10 17:54:40 -0600

    double checked that the nhanes_variable_list file was added to the package with the data.frame() format

--- R/nhanes_variable_list.R
+++ R/nhanes_variable_list.R
@@ -1,6 +1,5 @@
 
 #' This is data to be included in my package
-#'
 #' @author Herb Susmann
 #' @references \url{https://cran.r-project.org/web/packages/RNHANES/vignettes/introduction.html}
 "nhanes_variable_list"
