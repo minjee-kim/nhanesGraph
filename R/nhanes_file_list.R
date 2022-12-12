c92cf4b158ac43cc6df78222b9f9596fef2d286d
Minjee Kim <minjeekim0804@tamu.edu>
2022-12-11 21:25:29 -0600

    edited documentation for nhanes_file_list because the data is not used in the functions in R folder but the functions in inst folder

--- R/nhanes_file_list.R
+++ R/nhanes_file_list.R
@@ -2,6 +2,5 @@
 #' This is data to be included in my package
 #'
 #' @author Herb Susmann
-#' @docType data
 #' @references \url{https://cran.r-project.org/web/packages/RNHANES/vignettes/introduction.html}
 "nhanes_file_list"
